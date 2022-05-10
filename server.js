e98ca630c4fc2e7fb98164c31c75bd549a818379
Hengly Aun <aunhengly@gmail.com>
2022-05-10 11:36:52 -0700

    adding connection to database

--- server.js
+++ server.js
@@ -18,10 +18,8 @@ const image = require('./controllers/image');
 const db = knex ({
    client: 'pg',
    connection: {
-     host : 'postgresql-round-21063',
-     user : 'henglyaun',
-     password : '',
-     database : 'smart-brain'
+     host : process.env.DATABASE_URL,
+     ssl: true
    }
  });
 
